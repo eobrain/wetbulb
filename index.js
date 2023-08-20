5d2f485046a886aacbac1ca6498b7942f8502999
Eamonn O'Brien-Strain <eobrain@gmail.com>
2023-08-19 17:07:25 -0700

    Set up for iteration

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import openweathermap from './openweathermap.js'
 import wetbulb from './wetbulb.js'
 
-/* global $place $temp $humidity $sweatability */
+/* global $place $temp $humidity $sweatability $here $best $worst $better $worse */
 
 // All temperatures on Celcius
 
-const HUMAN_BODY_TEMP = 37
+const HUMAN_BODY_TEMP_C = 37
+const KM_IN_LAT_DEG = 0.008
 
 // const decimal = (degrees, minutes, seconds) => degrees + minutes / 60 + seconds / 3600
 // const FRANZ = { lat: decimal(38, 34, 54), lon: decimal(-122, 36, 36) }
@@ -17,20 +18,23 @@ async function show (latLon) {
 
   const { temp, humidity } = main
 
-  const wetBulbTemp = wetbulb(temp, humidity)
+  const wetBulbTempC = wetbulb(temp, humidity)
 
-  const sweatability = HUMAN_BODY_TEMP - wetBulbTemp
+  const sweatability = HUMAN_BODY_TEMP_C - wetBulbTempC
 
   $place.innerText = name
   $temp.innerText = Math.round(temp)
   $humidity.innerText = Math.round(humidity)
   $sweatability.innerText = Math.round(sweatability)
+  $better.disabled = false
+  $worse.disabled = false
 }
 
-navigator.geolocation.getCurrentPosition(async (position) => {
-  const { coords } = position
-  const { latitude: lat, longitude: lon } = coords
-  await show({ lat, lon })
-}, async () => {
-  await show(FRANZ)
-})
+$here.onclick = () =>
+  navigator.geolocation.getCurrentPosition(async (position) => {
+    const { coords } = position
+    const { latitude: lat, longitude: lon } = coords
+    await show({ lat, lon })
+  }, async () => {
+    await show(FRANZ)
+  })
