af457c0e04664b22aced774db791b623fdd29e6d
Eamonn O'Brien-Strain <eobrain@gmail.com>
2023-08-20 13:20:32 -0700

    Cache the geocoding

--- geocode.js
+++ geocode.js
@@ -1,4 +1,6 @@
-export default async ({ lat, lon }) => {
+const cache = new Map()
+
+const uncached = async ({ lat, lon }) => {
   try {
     const result = await fetch(`https://geocode.maps.co/reverse?lat=${lat}&lon=${lon}`)
     const { display_name: displayName } = await result.json()
@@ -6,4 +8,16 @@ export default async ({ lat, lon }) => {
   } catch (exp) {
     return undefined
   }
+}
+
+export default async (location) => {
+  const key = JSON.stringify(location)
+  if (cache.has(key)) {
+    console.log('HIT')
+    return cache.get(key)
+  }
+  console.log('MISS')
+  const result = uncached(location)
+  cache.set(key, result)
+  return result
 }
