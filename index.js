c4b5f8f1db8ec1b8c4d16f6d165390be75e154de
Eamonn O'Brien-Strain <eobrain@gmail.com>
2023-08-19 18:22:18 -0700

    Implement hillclimbing

--- index.js
+++ index.js
@@ -11,17 +11,23 @@ const KM_IN_LAT_DEG = 0.008
 // const decimal = (degrees, minutes, seconds) => degrees + minutes / 60 + seconds / 3600
 // const FRANZ = { lat: decimal(38, 34, 54), lon: decimal(-122, 36, 36) }
 
-const FRANZ = { lat: 38.581621, lon: -122.609887 }
+// const FRANZ = { lat: 38.581621, lon: -122.609887 }
 
-async function show (latLon) {
-  const { main, name } = await openweathermap(latLon)
+const place = {}
+let sweatabilityAtPlace
+
+const get = async (location) => {
+  const { main, name } = await openweathermap(location)
 
   const { temp, humidity } = main
 
   const wetBulbTempC = wetbulb(temp, humidity)
 
   const sweatability = HUMAN_BODY_TEMP_C - wetBulbTempC
+  return { name, temp, humidity, sweatability }
+}
 
+async function show ({ name, temp, humidity, sweatability }) {
   $place.innerText = name
   $temp.innerText = Math.round(temp)
   $humidity.innerText = Math.round(humidity)
@@ -33,8 +39,60 @@ async function show (latLon) {
 $here.onclick = () =>
   navigator.geolocation.getCurrentPosition(async (position) => {
     const { coords } = position
-    const { latitude: lat, longitude: lon } = coords
-    await show({ lat, lon })
-  }, async () => {
-    await show(FRANZ)
+    const { latitude, longitude } = coords
+    place.lat = latitude
+    place.lon = longitude
+    const { name, temp, humidity, sweatability } = await get(place)
+    sweatabilityAtPlace = sweatability
+    await show({ name, temp, humidity, sweatability })
+  }, async (error) => {
+    throw new Error(error)
   })
+
+const D = 5 * KM_IN_LAT_DEG
+const DELTAS = [[-D, 0], [D, 0], [0, -D], [0, D]]
+async function move (up) {
+  let optimalLatLon = place
+  let optimalSweatability = sweatabilityAtPlace
+  let optimalResult
+  for (const delta of DELTAS) {
+    const [dLat, dLon] = delta
+    const latLon = { lat: place.lat + dLat, lon: place.lon + dLon }
+    const result = await get(latLon)
+    const accept = !!((result.sweatability > optimalSweatability) ^ up)
+    if (accept) {
+      optimalLatLon = latLon
+      optimalSweatability = result.sweatability
+      optimalResult = result
+    }
+  }
+  if (optimalResult) {
+    place.lat = optimalLatLon.lat
+    place.lon = optimalLatLon.lon
+    sweatabilityAtPlace = optimalSweatability
+    await show(optimalResult)
+    return true
+  }
+  return false
+}
+
+const sleep = (delayMs) => new Promise((resolve) => setTimeout(resolve, delayMs))
+
+async function hillClimb (up) {
+  $here.disabled = true
+  $best.disabled = true
+  $worst.disabled = true
+  $better.disabled = true
+  $worse.disabled = true
+  while (await move(up)) {
+    await sleep(4000)
+  }
+  $here.disabled = false
+  $best.disabled = false
+  $worst.disabled = false
+  $better.disabled = false
+  $worse.disabled = false
+}
+
+$worse.onclick = () => hillClimb(true)
+$better.onclick = () => hillClimb(false)
