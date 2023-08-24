753d1c40155d86f930e7e7e15675de0496ea388c
Eamonn O'Brien-Strain <eobrain@gmail.com>
2023-08-24 05:18:26 +0000

    Refactor world map into a separate module

--- index.js
+++ index.js
@@ -2,71 +2,16 @@ import openweathermap from './openweathermap.js'
 import wetbulb from './wetbulb.js'
 import geocode from './geocode.js'
 import { anneal, currentPlace } from './optimize.js'
+import { drawDot } from './world.js'
 
-/* global ol $place $temp $tempF $humidity $guage $sweatability  $worst */
+/* global $place $temp $tempF $humidity $guage $sweatability  $worst */
 const $hygrometer = document.getElementById('hygrometer')
 const $thermometer = document.getElementById('thermometer')
 
 const HUMAN_BODY_TEMP_C = 37
 
-const map = new ol.Map({
-  target: 'map',
-  layers: [
-    new ol.layer.Tile({
-      source: new ol.source.OSM()
-    })
-  ],
-  view: new ol.View({
-    center: ol.proj.fromLonLat([-75, 35]),
-    zoom: 2
-  })
-})
-
 const MAX_S = 32
 const MIN_S = 0
-
-const hexByte = n => ('00' + n.toString(16).toUpperCase()).slice(-2)
-
-const color = sweatability => {
-  let twiceByte = Math.trunc(0x200 * (sweatability - MIN_S) / (MAX_S - MIN_S))
-  if (twiceByte < 0) {
-    twiceByte = 0
-  }
-  if (twiceByte > 0x1FF) {
-    twiceByte = 0x1FF
-  }
-  let red = 0xFF
-  let green = 0xFF
-  if (twiceByte <= 0xFF) {
-    green = twiceByte
-  } else {
-    red = 0x200 - twiceByte
-  }
-  return '#' + hexByte(red) + hexByte(green) + '00'
-}
-
-function drawDot ({ lat, lon }, sweatability) {
-  const features = []
-  features.push(new ol.Feature({
-    geometry: new ol.geom.Point(ol.proj.fromLonLat([
-      lon, lat
-    ]))
-  }))
-  // create the source and layer for random features
-  const vectorSource = new ol.source.Vector({
-    features
-  })
-  const vectorLayer = new ol.layer.Vector({
-    source: vectorSource,
-    style: new ol.style.Style({
-      image: new ol.style.Circle({
-        radius: 10,
-        fill: new ol.style.Fill({ color: color(sweatability) })
-      })
-    })
-  })
-  map.addLayer(vectorLayer)
-}
 
 const uncachedGet = async (location) => {
   const name = await geocode(location)
@@ -125,7 +70,7 @@ function guageVariables (sweatability) {
 
 async function show ({ name, temp, humidity, sweatability }) {
   const place = currentPlace()
-  drawDot(place, sweatability)
+  drawDot(place, sweatability, MIN_S, MAX_S)
   $place.innerText = name || `${place.lat},${place.lon}`
   $place.href = mapUrl(place)
   $temp.innerText = Math.round(temp)
