f14c8e78f1c8d84d9a37587caab46cda638c1b29
Eamonn O'Brien-Strain <eobrain@gmail.com>
2023-08-24 05:06:52 +0000

    Refactor to put optimization in separate module

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 import openweathermap from './openweathermap.js'
 import wetbulb from './wetbulb.js'
 import geocode from './geocode.js'
+import { anneal, currentPlace } from './optimize.js'
 
-/* global ol $place $temp $tempF $humidity $guage $sweatability $here $best $worst $better $worse */
+/* global ol $place $temp $tempF $humidity $guage $sweatability  $worst */
 const $hygrometer = document.getElementById('hygrometer')
 const $thermometer = document.getElementById('thermometer')
 
 const HUMAN_BODY_TEMP_C = 37
-const KM_IN_LAT_DEG = 0.008
-
-const place = {}
-let sweatabilityAtPlace
 
 const map = new ol.Map({
   target: 'map',
@@ -127,6 +124,7 @@ function guageVariables (sweatability) {
 }
 
 async function show ({ name, temp, humidity, sweatability }) {
+  const place = currentPlace()
   drawDot(place, sweatability)
   $place.innerText = name || `${place.lat},${place.lon}`
   $place.href = mapUrl(place)
@@ -140,142 +138,10 @@ async function show ({ name, temp, humidity, sweatability }) {
   $sweatability.innerText = Math.round(sweatability)
   $hygrometer.value = humidity
   $thermometer.value = temp
-  $better.disabled = false
-  $worse.disabled = false
 }
 
-$here.onclick = () =>
-  navigator.geolocation.getCurrentPosition(async (position) => {
-    const { coords } = position
-    const { latitude, longitude } = coords
-    place.lat = latitude
-    place.lon = longitude
-    const result = await get(place)
-    if (!result) {
-      throw new Error(`${mapUrl(place)} not a known place`)
-    }
-    const { name, temp, humidity, sweatability } = result
-    sweatabilityAtPlace = sweatability
-    await show({ name, temp, humidity, sweatability })
-  }, async (error) => {
-    throw new Error(error)
-  })
-
-const randomElement = array => array[Math.floor(Math.random() * array.length)]
-
-const D = KM_IN_LAT_DEG
-const DELTAS = [
-  [-D, -D], [-D, 0], [-D, D],
-  [0, -D], [0, D],
-  [D, -D], [D, 0], [D, D]
-]
-async function hillclimbMove (up) {
-  let optimalLatLon = place
-  let optimalSweatability = sweatabilityAtPlace
-  let optimalResult
-  for (const delta of DELTAS) {
-    const [dLat, dLon] = delta
-    const latLon = { lat: place.lat + dLat, lon: place.lon + dLon }
-    const result = await get(latLon)
-    if (!result) {
-      continue
-    }
-    const dSweatability = result.sweatability - optimalSweatability
-    const dImprovement = up ? -dSweatability : dSweatability
-    const accept = dImprovement > 0
-    if (accept) {
-      optimalLatLon = latLon
-      optimalSweatability = result.sweatability
-      optimalResult = result
-    }
-  }
-  if (optimalResult) {
-    place.lat = optimalLatLon.lat
-    place.lon = optimalLatLon.lon
-    sweatabilityAtPlace = optimalSweatability
-    await show(optimalResult)
-    return true
-  }
-  return false
-}
-
-const latMod = lon => lon > 90 ? lon - 180 : (lon < -90 ? lon + 180 : lon)
-const lonMod = lon => lon > 180 ? lon - 360 : (lon < -180 ? lon + 360 : lon)
-
-async function annealMove (up, annealT, scale) {
-  const delta = randomElement(DELTAS)
-  const [dLat, dLon] = delta
-  const latLon = {
-    lat: latMod(place.lat + dLat * scale),
-    lon: lonMod(place.lon + dLon * scale)
-  }
-
-  const result = await get(latLon)
-  if (!result) {
-    return false
-  }
-  const dSweatability = result.sweatability - sweatabilityAtPlace
-  const dImprovement = up ? -dSweatability : dSweatability
-  const accept = Math.exp(dImprovement / annealT) > Math.random()
-  if (accept) {
-    place.lat = latLon.lat
-    place.lon = latLon.lon
-    sweatabilityAtPlace = result.sweatability
-    await show(result)
-    return true
-  }
-  return false
-}
-
-const sleep = (delayMs) => new Promise((resolve) => setTimeout(resolve, delayMs))
-
-async function hillClimb (up) {
-  $here.disabled = true
-  $best.disabled = true
+$worst.onclick = () => {
   $worst.disabled = true
-  $better.disabled = true
-  $worse.disabled = true
-  while (await hillclimbMove(up)) {
-    await sleep(4000)
-  }
-  $here.disabled = false
-  $best.disabled = false
+  anneal(get, show)
   $worst.disabled = false
-  $better.disabled = false
-  $worse.disabled = false
 }
-
-async function anneal (up) {
-  let result
-  while (!result) {
-    place.lat = Math.random() * 180 - 90
-    place.lon = Math.random() * 360 - 180
-    result = await get(place)
-  }
-  sweatabilityAtPlace = result.sweatability
-  await show(result)
-
-  $here.disabled = true
-  $best.disabled = true
-  $worst.disabled = true
-  $better.disabled = true
-  $worse.disabled = true
-  for (let scale = 8192; scale >= 1; scale /= 2) {
-    const annealT = 5 * (scale / 8192) ** 0.5
-    console.log({ scale, annealT })
-    for (let i = 0; i < 100; ++i) {
-      await annealMove(up, annealT, scale)
-      await sleep(200)
-    }
-  }
-  $here.disabled = false
-  $best.disabled = false
-  $worst.disabled = false
-  $better.disabled = false
-  $worse.disabled = false
-}
-
-$worst.onclick = () => anneal(true)
-$best.onclick = () => anneal(false)
-$worse.onclick = () => hillClimb(true)
-$better.onclick = () => hillClimb(false)
---
+++ optimize.js
@@ -0,0 +1,63 @@
+const randomElement = array => array[Math.floor(Math.random() * array.length)]
+
+const KM_IN_LAT_DEG = 0.008
+const D = KM_IN_LAT_DEG
+const DELTAS = [
+  [-D, -D], [-D, 0], [-D, D],
+  [0, -D], [0, D],
+  [D, -D], [D, 0], [D, D]
+]
+
+const latMod = lon => lon > 90 ? lon - 180 : (lon < -90 ? lon + 180 : lon)
+const lonMod = lon => lon > 180 ? lon - 360 : (lon < -180 ? lon + 360 : lon)
+
+const place = {}
+let sweatabilityAtPlace
+
+export const currentPlace = () => place
+
+async function annealMove (annealT, scale, get, show) {
+  const delta = randomElement(DELTAS)
+  const [dLat, dLon] = delta
+  const latLon = {
+    lat: latMod(place.lat + dLat * scale),
+    lon: lonMod(place.lon + dLon * scale)
+  }
+
+  const result = await get(latLon)
+  if (!result) {
+    return false
+  }
+  const dImprovement = sweatabilityAtPlace - result.sweatability
+  const accept = Math.exp(dImprovement / annealT) > Math.random()
+  if (accept) {
+    place.lat = latLon.lat
+    place.lon = latLon.lon
+    sweatabilityAtPlace = result.sweatability
+    await show(result)
+    return true
+  }
+  return false
+}
+
+const sleep = (delayMs) => new Promise((resolve) => setTimeout(resolve, delayMs))
+
+export async function anneal (get, show) {
+  let result
+  while (!result) {
+    place.lat = Math.random() * 180 - 90
+    place.lon = Math.random() * 360 - 180
+    result = await get(place)
+  }
+  sweatabilityAtPlace = result.sweatability
+  await show(result)
+
+  for (let scale = 8192; scale >= 1; scale /= 2) {
+    const annealT = 5 * (scale / 8192) ** 0.5
+    console.log({ scale, annealT })
+    for (let i = 0; i < 100; ++i) {
+      await annealMove(annealT, scale, get, show)
+      await sleep(200)
+    }
+  }
+}
