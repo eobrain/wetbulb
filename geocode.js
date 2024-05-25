50d46f1d1e022935c83acf4b611362aace88b69c
Eamonn O'Brien-Strain <eobrain@gmail.com>
2024-05-25 12:07:04 -0700

    throttle geocode

--- geocode.js
+++ geocode.js
@@ -1,9 +1,12 @@
 const cache = new Map()
 
+const sleep = ms => new Promise(r => setTimeout(r, ms))
+
 const uncached = async ({ lat, lon }) => {
   try {
-    const result = await fetch(`https://geocode.maps.co/reverse?lat=${lat}&lon=${lon}`)
+    const result = await fetch(`https://geocode.maps.co/reverse?lat=${lat}&lon=${lon}&api_key=6640557992233270780261vlg1a4926`)
     const { display_name: displayName } = await result.json()
+    await sleep(5000) // rate limit is 1 request/sec
     return displayName
   } catch (exp) {
     return undefined
