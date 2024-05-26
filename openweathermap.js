a26278a50bbe305ecaa11e901c8817c730d145a2
Eamonn O'Brien-Strain <eobrain@gmail.com>
2024-05-26 09:37:58 -0700

    Use the worst wetbulb temperature

--- openweathermap.js
+++ openweathermap.js
@@ -8,10 +8,12 @@ const cached = async ({ lat, lon }) => {
 
 export default async ({ lat, lon }) => {
   const result = await cached({ lat, lon })
+  const forecasts = result.list.map(f => { f.main.wetbulb = wetbulb(f.main.temp, f.main.humidity); return f })
+  const worstForecast = forecasts.reduce((worstF, f) =>
+    worstF.main.wetbulb > f.main.wetbulb ? worstF : f)
   const name = result.city.name
   const population = result.city.population
-  const description = `${result.city.country} ${result.list[0].dt_txt} ${result.list[0].weather[0].description}`
-  const main = result.list[0].main
-  main.wetbulb = wetbulb(main.temp, main.humidity)
+  const description = `${result.city.country} ${worstForecast.dt_txt} ${worstForecast.weather[0].description}`
+  const main = worstForecast.main
   return { name, population, description, main }
 }
